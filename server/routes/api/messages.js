d35312000f830952c6b0bd7f8c82ad83aadb101f
eopo <26900313+eopo@users.noreply.github.com>
2025-04-17 23:09:52 +0200

    Move GET /messages/:id into new API file and un-callback-hell #Fruehjahrsputz "

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -6,13 +6,32 @@ const logger = require('../../log');
 
 const router = express.Router();
 
+function getMessageQuery(req) {
+        const pdwMode = nconf.get('messages:pdwMode');
+        const adminShow = nconf.get('messages:adminShow');
+        const queryTemplate = db.from('messages').modify(queryBuilder => {
+                if (!req.isAuthenticated()) queryBuilder.where('capcodes.onlyShowLoggedIn', false);
+
+                if (pdwMode && !(adminShow && req.isAuthenticated() && req.user.role === 'admin')) {
+                        queryBuilder
+                                .innerJoin('capcodes', 'capcodes.id', '=', 'messages.alias_id')
+                                .where('capcodes.ignore', 0);
+                } else {
+                        queryBuilder.leftJoin('capcodes', 'capcodes.id', '=', 'messages.alias_id').where(qb => {
+                                qb.where('capcodes.ignore', 0).orWhereNull('capcodes.ignore');
+                        });
+                }
+        });
+        return queryTemplate.clone();
+}
+
 router.route('/messages').get(authHelper.isLoggedInMessages, async function(req, res) {
         try {
-                const pdwMode = nconf.get('messages:pdwMode');
-                const adminShow = nconf.get('messages:adminShow');
+                const HideCapcode = nconf.get('messages:HideCapcode');
+
                 const maxLimit = nconf.get('messages:maxLimit');
                 const defaultLimit = nconf.get('messages:defaultLimit');
-                const HideCapcode = nconf.get('messages:HideCapcode');
+
                 const replaceText = nconf.get('messages:replaceText');
 
                 const initData = {
@@ -33,22 +52,7 @@ router.route('/messages').get(authHelper.isLoggedInMessages, async function(req,
                         initData.limit = parseInt(defaultLimit, 10);
                 }
 
-                const queryTemplate = db.from('messages').modify(queryBuilder => {
-                        if (!req.isAuthenticated()) queryBuilder.where('capcodes.onlyShowLoggedIn', false);
-
-                        if (pdwMode && !(adminShow && req.isAuthenticated() && req.user.role === 'admin')) {
-                                queryBuilder
-                                        .innerJoin('capcodes', 'capcodes.id', '=', 'messages.alias_id')
-                                        .where('capcodes.ignore', 0);
-                        } else {
-                                queryBuilder
-                                        .leftJoin('capcodes', 'capcodes.id', '=', 'messages.alias_id')
-                                        .where('capcodes.ignore', 0)
-                                        .orWhereNull('capcodes.ignore');
-                        }
-                });
-
-                const initCount = await queryTemplate.clone().count('* as msgcount');
+                const initCount = await getMessageQuery(req).count('* as msgcount');
 
                 const count = initCount[0].msgcount;
                 if (count === undefined) throw new Error('Something went really really wrong!'); // TODO: How the fuck would we end up in this situation?
@@ -86,8 +90,7 @@ router.route('/messages').get(authHelper.isLoggedInMessages, async function(req,
                                 )
                         );
 
-                const messages = await queryTemplate
-                        .clone()
+                const messages = await getMessageQuery(req)
                         .select(fields)
                         .orderBy('messages.timestamp', 'desc')
                         .limit(initData.limit)
@@ -96,7 +99,47 @@ router.route('/messages').get(authHelper.isLoggedInMessages, async function(req,
                 res.status(200).json({ init: initData, messages });
         } catch (err) {
                 logger.main.error(err);
-                res.status(200).json({ init: {}, messages: [] });
+                res.status(500).json({ init: {}, messages: [] });
+        }
+});
+
+router.route('/messages/:messageId').get(authHelper.isLoggedInMessages, async function(req, res) {
+        try {
+                const messageId = parseInt(req.params.messageId, 10);
+                const HideCapcode = nconf.get('messages:HideCapcode');
+
+                const fields = [
+                        'messages.id',
+                        'messages.message',
+                        'messages.timestamp',
+                        'messages.timestamp as datetime',
+                        'capcodes.alias',
+                        'capcodes.agency',
+                        'capcodes.icon',
+                        'capcodes.color',
+                ];
+
+                if (!nconf.get('messages:HideSource') || (req.isAuthenticated() && req.user.role === 'admin'))
+                        fields.push('messages.source');
+
+                if (!HideCapcode || req.isAuthenticated()) fields.push('messages.address');
+
+                if (req.isAuthenticated() && req.user.role === 'admin')
+                        fields.push(
+                                db.raw(
+                                        'CASE WHEN NOT capcodes.address = messages.address THEN 1 ELSE 0 END as wildcard'
+                                )
+                        );
+
+                const message = await getMessageQuery(req)
+                        .select(fields)
+                        .andWhere('messages.id', messageId)
+                        .first();
+
+                res.status(200).json(message || {});
+        } catch (err) {
+                logger.main.error(err);
+                res.status(500).json({});
         }
 });
 

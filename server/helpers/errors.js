32dea15c0421c87529a9ddfbef563079123d26f2
eopo <26900313+eopo@users.noreply.github.com>
2025-04-23 22:58:48 +0200

    Add  new error classes

--- server/helpers/errors.js
+++ server/helpers/errors.js
@@ -1,53 +1,50 @@
 class NotAuthorizedError extends Error {
         constructor(message, options) {
                 super(message || 'You are not authorized to access this resource', options);
-
-                // assign the error class name in your custom error (as a shortcut)
                 this.name = this.constructor.name;
                 this.status = 403;
-
-                // capturing the stack trace keeps the reference to your error class
                 Error.captureStackTrace(this, this.constructor);
-
-                // you may also assign additional properties to your error
-                this.isSleepy = true;
         }
 }
 
 class NotAuthenticatedError extends Error {
         constructor(message, options) {
                 super(message || 'Not authorized', options);
-
-                // assign the error class name in your custom error (as a shortcut)
                 this.name = this.constructor.name;
                 this.status = 401;
-
-                // capturing the stack trace keeps the reference to your error class
                 Error.captureStackTrace(this, this.constructor);
-
-                // you may also assign additional properties to your error
-                this.isSleepy = true;
         }
 }
 
 class InvalidRequestError extends Error {
         constructor(message, options) {
                 super(message || 'Invalid request', options);
-
-                // assign the error class name in your custom error (as a shortcut)
                 this.name = this.constructor.name;
                 this.status = 400;
-
-                // capturing the stack trace keeps the reference to your error class
                 Error.captureStackTrace(this, this.constructor);
+        }
+}
+class ResourceNotFoundError extends Error {
+        constructor(message, options) {
+                super(message || 'Resource not found', options);
+                this.name = this.constructor.name;
+                this.status = 404;
+                Error.captureStackTrace(this, this.constructor);
+        }
+}
 
-                // you may also assign additional properties to your error
-                this.isSleepy = true;
+class RequiredFieldMissingError extends InvalidRequestError {
+        constructor(message, options) {
+                super(message ? `Required field ${message} is missing` : 'Required field missing', options);
+                this.name = this.constructor.name;
+                Error.captureStackTrace(this, this.constructor);
         }
 }
 
 module.exports = {
         NotAuthorizedError,
         NotAuthenticatedError,
-        InvalidRequestError
+        InvalidRequestError,
+        ResourceNotFoundError,
+        RequiredFieldMissingError,
 };

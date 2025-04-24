28952e3be3ea0992f94789eb736b93b08490ef48
eopo <26900313+eopo@users.noreply.github.com>
2025-04-24 10:44:54 +0200

    Clean up knexfile.js #Fruehjahrsputz

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,7 +3,7 @@ const logger = require('./log');
 
 const debug = nconf.get('global:loglevel') === 'debug';
 
-var dbtype = nconf.get('database:type');
+const dbtype = nconf.get('database:type');
 
 // in order to create migration files, client must be hardcoded to 'sqlite3' otherwise it won't work.
 const dbconfig = {
@@ -36,20 +36,28 @@ const dbconfig = {
 if (process.env.NODE_ENV === 'test') {
         dbconfig.client = 'sqlite3'; // Always use sqlite3 for tests, regardless what is being set in config (for now)
         dbconfig.connection.filename = './test/messages.db';
-} else if (dbtype === 'sqlite3') {
-        dbconfig.connection.filename = nconf.get('database:file');
-} else if (dbtype === 'mysql') {
-        dbconfig.connection.host = nconf.get('database:server');
-        dbconfig.connection.port = nconf.get('database:port');
-        dbconfig.connection.user = nconf.get('database:username');
-        dbconfig.connection.password = nconf.get('database:password');
-        dbconfig.connection.database = nconf.get('database:database');
-} else if (dbtype === 'oracledb') {
-        dbconfig.connection.connectString = nconf.get('database:connectString');
-        dbconfig.connection.user = nconf.get('database:username');
-        dbconfig.connection.password = nconf.get('database:password');
-        dbconfig.fetchAsString = ['clob'];
-} // TODO: we have mssql and postgres in other parts of the code, but it's not included here.
+} else
+        switch (dbtype) {
+                case 'sqlite3':
+                        dbconfig.connection.filename = nconf.get('database:file');
+                        break;
+                case 'mysql':
+                        dbconfig.connection.host = nconf.get('database:server');
+                        dbconfig.connection.port = nconf.get('database:port');
+                        dbconfig.connection.user = nconf.get('database:username');
+                        dbconfig.connection.password = nconf.get('database:password');
+                        dbconfig.connection.database = nconf.get('database:database');
+                        break;
+                case 'oracledb':
+                        dbconfig.connection.connectString = nconf.get('database:connectString');
+                        dbconfig.connection.user = nconf.get('database:username');
+                        dbconfig.connection.password = nconf.get('database:password');
+                        dbconfig.fetchAsString = ['clob'];
+                        break;
+                default:
+                        throw new Error(`Unsupported database type: ${dbtype}`);
+        }
+// TODO: we have mssql and postgres in other parts of the code, but it's not included here.
 
 // this is required because of the silly way knex migrations handle environments
 module.exports = Object.assign({}, dbconfig, {

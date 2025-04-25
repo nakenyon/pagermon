ae045f2b7467026589aa5bd70e27c7498ba44f74
eopo <26900313+eopo@users.noreply.github.com>
2025-04-25 20:09:11 +0200

    Lint and un-callbackhell Telegram.js

--- server/plugins/Telegram.js
+++ server/plugins/Telegram.js
@@ -1,39 +1,36 @@
-var telegram = require('telegram-bot-api');
-var util = require('util');
-var logger = require('../log');
+const TelegramApi = require('telegram-bot-api');
+const util = require('util');
+const logger = require('../log');
 
-function run(trigger, scope, data, config, callback) {
-    var tConf = data.pluginconf.Telegram;
-    if (tConf && tConf.enable) {
-        var telekey = config.teleAPIKEY;
-        var t = new telegram({
-          token: telekey
-        });
-        if (tConf.chat == 0 || !tConf.chat) {
-            logger.main.error('Telegram: ' + data.address + ' No ChatID key set. Please enter ChatID.');
-            callback();
-        } else {
-            //Notification formatted in Markdown for pretty notifications
-            var notificationText = `*${data.agency} - ${data.alias}*\n` + 
-                                    `Message: ${data.message}`;
-            
-            t.sendMessage({
-                chat_id: tConf.chat,
-                text: notificationText,
-                parse_mode: "Markdown"
-            }).then(function(data) {
-                logger.main.debug('Telegram: ' + util.inspect(data, false, null));
+async function run(trigger, scope, data, config, callback) {
+        try {
+                const tConf = data.pluginconf.Telegram;
+                if (!tConf || !tConf.enable) return callback();
+                const telegramApiKey = config.teleAPIKEY;
+                const telegram = new TelegramApi({
+                        token: telegramApiKey,
+                });
+                if (tConf.chat === 0 || !tConf.chat) {
+                        logger.main.error(`Telegram: ${data.address} No ChatID key set. Please enter ChatID.`);
+                        return callback();
+                }
+                // Notification formatted in Markdown for pretty notifications
+                const notificationText = `*${data.agency} - ${data.alias}*\nMessage: ${data.message}`;
+
+                const responseData = await telegram.sendMessage({
+                        chat_id: tConf.chat,
+                        text: notificationText,
+                        parse_mode: 'Markdown',
+                });
+
+                logger.main.debug(`Telegram: ${util.inspect(responseData, false, null)}`);
                 callback();
-            }).catch(function(err) {
-                logger.main.error('Telegram: ' + err);
+        } catch (error) {
+                logger.main.error(`Telegram: ${error}`);
                 callback();
-            });
         }
-    } else {
-        callback();
-    }
 }
 
 module.exports = {
-    run: run
-}
+        run,
+};
